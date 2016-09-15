8db113368fca84ea47dc7ebd41e5747b95c297b0
Matt Marjanovic <maddog@tri.global>
2016-09-15 14:29:41 -0700

    Write some documentation.

--- drake/automotive/maliput/geometry_api/junction.h
+++ drake/automotive/maliput/geometry_api/junction.h
@@ -8,17 +8,30 @@ namespace geometry_api {
 class RoadGeometry;
 class Segment;
 
+/// A Junction is a closed set of Segments which physically overlap,
+/// in the sense that RoadPositions in the domains of two Segments map
+/// to the same GeoPosition.  The Segments need not be directly
+/// connected to one another in the network topology.
+///
+/// Junctions are owned/managed by RoadGeometry.
 class Junction {
  public:
   virtual ~Junction() {}
 
-  // Provide persistent ID.
+  /// @return the persistent identifier.
+  ///
+  // TODO(maddog)  Tie id into a tiling mechanism?
   virtual const JunctionId id() const = 0;
 
+  /// @return the RoadGeometry which owns this Junction.
   virtual const RoadGeometry* road_geometry() const = 0;
 
+  // TODO(maddog)  Real iteration support over junctions and branch-points.
+
+  /// @return the number of Segments in the Junction.
   virtual int num_segments() const = 0;
 
+  /// @return the Segment indexed by @param index.
   virtual const Segment* segment(int index) const = 0;
 };
 
--- drake/automotive/maliput/geometry_api/road_geometry.h
+++ drake/automotive/maliput/geometry_api/road_geometry.h
@@ -8,22 +8,35 @@ namespace geometry_api {
 class BranchPoint;
 class Junction;
 
+/// Abstract API for the geometry of a road network, including both
+/// the network topology and the geometry of its embedding in 3-space.
 class RoadGeometry {
  public:
   virtual ~RoadGeometry() {}
 
-  // Provide persistent ID.
+  /// @return the persistent identifier.
+  ///
+  // TODO(maddog)  Tie id into a tiling mechanism?
   virtual const RoadGeometryId id() const = 0;
 
+  // TODO(maddog)  Real iteration support over junctions and branch-points.
+
+  /// @return the number of Junctions in the RoadGeometry.
   virtual int num_junctions() const = 0;
 
+  /// @return the Junction indexed by @param index.
   virtual const Junction* junction(int index) const = 0;
 
+  /// @return the number of BrancPoints n the RoadGeometry.
   virtual int num_branch_points() const = 0;
 
+  /// @return the BranchPoint indexed by @param index.
   virtual const BranchPoint* branch_point(int index) const = 0;
 
 
+  /// Figure out the RoadPosition corresponding to GeoPosition @param geo_pos,
+  /// potentially using @param hint to guide the search.
+  // TODO(maddog)  UNDER CONSTRUCTION
   virtual RoadPosition ToRoadPosition(const GeoPosition& geo_pos,
                                       const RoadPosition& hint) const = 0;
 };
