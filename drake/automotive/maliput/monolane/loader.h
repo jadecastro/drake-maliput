ab1d8c349b946f94b7053977685deae650f9860d
Rick Poyner <rick.poyner@tri.global>
2016-09-30 11:11:49 -0400

    WIP: move yaml parsing into new module monolane/loader. No unit tests yet.

---
+++ drake/automotive/maliput/monolane/loader.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <memory>
+#include <string>
+
+#include "drake/drakeAutomotive_export.h"
+
+namespace maliput {
+
+namespace geometry_api {
+class RoadGeometry;
+}
+
+namespace monolane {
+
+namespace api = maliput::geometry_api;
+
+/// Loader for serialized monolane road networks.
+///
+/// The serialization is a fairly straightforward mapping of the Builder
+/// interface onto YAML. See (TBD) for more detail of the format.
+
+/// Load the input string as a maliput_monolane_builder document.
+std::unique_ptr<const api::RoadGeometry> DRAKEAUTOMOTIVE_EXPORT
+Load(const std::string& input);
+
+/// Load the named file as a maliput_monolane_builder document.
+std::unique_ptr<const api::RoadGeometry> DRAKEAUTOMOTIVE_EXPORT
+LoadFile(const std::string& filename);
+
+}  // namespace monolane
+}  // namespace maliput
--- drake/automotive/maliput/utility/yaml_to_obj.cc
+++ drake/automotive/maliput/utility/yaml_to_obj.cc
@@ -2,15 +2,12 @@
 ///
 /// Take a yaml file as input, build the resulting monolane road geometry, and
 /// render the road surface to a WaveFront OBJ output file.
-#include <cmath>
 #include <iostream>
 #include <string>
 
 #include <gflags/gflags.h>
-#include "yaml-cpp/yaml.h"
 
-#include "drake/common/drake_assert.h"
-#include "drake/automotive/maliput/monolane/builder.h"
+#include "drake/automotive/maliput/monolane/loader.h"
 #include "drake/automotive/maliput/utility/generate_obj.h"
 
 namespace api = maliput::geometry_api;
@@ -22,104 +19,6 @@ DEFINE_string(obj_file, "yaml_to_obj.obj",
               "WaveFront OBJ output file"
               " to contain the rendered road surface");
 
-api::RBounds rbounds(const YAML::Node& node) {
-  DRAKE_DEMAND(node.IsSequence());
-  DRAKE_DEMAND(node.size() == 2);
-  return {node[0].as<double>(), node[1].as<double>()};
-}
-
-double d2r(double degrees) {
-  return degrees * M_PI / 180.;
-}
-
-mono::XYPoint xypoint(const YAML::Node& node) {
-  DRAKE_DEMAND(node.IsSequence());
-  DRAKE_DEMAND(node.size() == 3);
-  return {
-    node[0].as<double>(), node[1].as<double>(), d2r(node[2].as<double>())};
-}
-
-mono::ZPoint zpoint(const YAML::Node& node) {
-  DRAKE_DEMAND(node.IsSequence());
-  DRAKE_DEMAND(node.size() == 4);
-  return {
-    node[0].as<double>(), node[1].as<double>(),
-        d2r(node[2].as<double>()), d2r(node[3].as<double>())};
-}
-
-mono::ArcOffset arc_offset(const YAML::Node& node) {
-  DRAKE_DEMAND(node.IsSequence());
-  DRAKE_DEMAND(node.size() == 2);
-  return {node[0].as<double>(), d2r(node[1].as<double>())};
-}
-
-
-
-const mono::Connection* maybe_make_connection(
-    std::string id,
-    YAML::Node node,
-    const std::map<std::string, mono::XYZPoint>& xyzpoints,
-    mono::Builder* builder) {
-  DRAKE_DEMAND(node.IsMap());
-
-  // Check if needed symbols are available.
-  auto start_symbol = node["start"].as<std::string>();
-  auto it_start = xyzpoints.find(start_symbol);
-  if (it_start == xyzpoints.end()) { return nullptr; }
-
-  // Discover segment type.
-  bool forced_end = false;
-  auto it_fe = xyzpoints.end();
-  enum SegmentType{ line, arc } segment_type{};
-  for (const auto& kv : node) {
-    std::string key = kv.first.as<std::string>();
-
-    if (key == "length") {
-      segment_type = line;
-    } else if (key == "arc") {
-      segment_type = arc;
-    } else if (key == "forced_end") {
-      forced_end = true;
-      // More symbol resolution.
-      // TODO(rico): fold this and above into a function.
-      auto fe_symbol = node["forced_end"].as<std::string>();
-      it_fe = xyzpoints.find(fe_symbol);
-      if (it_fe == xyzpoints.end()) { return nullptr; }
-    }
-  }
-
-  // Call appropriate method.
-  switch (segment_type) {
-    case line: {
-      if (forced_end) {
-        return builder->Connect(
-            id, it_start->second, node["length"].as<double>(),
-            zpoint(node["z_end"]), it_fe->second.xy_);
-      } else {
-        return builder->Connect(
-            id, it_start->second, node["length"].as<double>(),
-            zpoint(node["z_end"]));
-      }
-    }
-    case arc: {
-      if (forced_end) {
-        return builder->Connect(id, it_start->second, arc_offset(node["arc"]),
-                                zpoint(node["z_end"]), it_fe->second.xy_);
-      } else {
-        return builder->Connect(id, it_start->second, arc_offset(node["arc"]),
-                                zpoint(node["z_end"]));
-      }
-    }
-  }
-
-  return nullptr;
-}
-
-mono::XYZPoint xyzpoint(const YAML::Node& node) {
-  DRAKE_DEMAND(node.IsMap());
-  return {xypoint(node["xypoint"]), zpoint(node["zpoint"])};
-}
-
 int main(int argc, char* argv[]) {
   std::cerr << "main() !\n";
   gflags::ParseCommandLineFlags(&argc, &argv, true);
@@ -128,54 +27,9 @@ int main(int argc, char* argv[]) {
     std::cerr << "no input file!\n";
     return 1;
   }
-  YAML::Node road_def = YAML::LoadFile(FLAGS_yaml_file);
+  std::cerr << "loading road geometry !\n";
+  auto rg = maliput::monolane::LoadFile(FLAGS_yaml_file);
 
-  DRAKE_DEMAND(road_def.IsMap());
-  YAML::Node mmb = road_def["maliput_monolane_builder"];
-  DRAKE_DEMAND(mmb.IsMap());
-
-  mono::Builder b(rbounds(mmb["lane_bounds"]),
-                  rbounds(mmb["driveable_bounds"]));
-
-  std::cerr << "loading points !\n";
-  YAML::Node points = mmb["points"];
-  DRAKE_DEMAND(points.IsMap());
-  std::map<std::string, mono::XYZPoint> xyzpoints;
-  for (const auto& p : points) {
-    xyzpoints[std::string("points.") + p.first.as<std::string>()] =
-        xyzpoint(p.second);
-  }
-
-  std::cerr << "loading raw connections !\n";
-  YAML::Node connections = mmb["connections"];
-  DRAKE_DEMAND(connections.IsMap());
-  std::map<std::string, YAML::Node> raw_connections;
-  for (const auto& c : connections) {
-    raw_connections[c.first.as<std::string>()] = c.second;
-  }
-
-  std::cerr << "building cooked connections !\n";
-  std::map<std::string, const mono::Connection*> cooked_connections;
-  while (!raw_connections.empty()) {
-    std::cerr << "raw count " << raw_connections.size()
-              << " cooked count " << cooked_connections.size() << "\n";
-    size_t cooked_before_this_pass = cooked_connections.size();
-    for (const auto& r : raw_connections) {
-      std::string id = r.first;
-      const mono::Connection* conn =
-          maybe_make_connection(id, r.second, xyzpoints, &b);
-      if (!conn) { continue; }
-      cooked_connections[id] = conn;
-      xyzpoints[std::string("connections.") + id + ".end"] = conn->end();
-    }
-    DRAKE_DEMAND(cooked_connections.size() > cooked_before_this_pass);
-    for (const auto& c : cooked_connections) {
-      raw_connections.erase(c.first);
-    }
-  }
-
-  std::cerr << "building road geometry !\n";
-  auto rg = b.Build({mmb["id"].Scalar()});
 
   std::cerr << "generating obj !\n";
   maliput::utility::generate_obj(rg.get(), FLAGS_obj_file, 1.);
